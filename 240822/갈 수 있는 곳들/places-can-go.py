03a3b4d8ab2fcf97f03c82c39346c42656260500
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-08-22 15:55:03 +0900

    Title: 갈 수 있는 곳들, Time: 239ms, Memory: 27MB, Status: Passed - CodeTree

--- 240822/갈 수 있는 곳들/places-can-go.py
+++ 240822/갈 수 있는 곳들/places-can-go.py
@@ -1,14 +1,41 @@
-import deque from collection
+from collections import deque 
 
-a = deque()
 n,k = map(int,input().split())
 arr = [list(map(int,input().split())) for _ in range(n)]
 check = [list(False for _ in range(n)) for _ in range(n)]
+answer = [list(0 for _ in range(n)) for _ in range(n)]
 position = [list(map(int,input().split())) for _ in range(k)] # 1개씩 작게 설정
-ans = 0
+order = 0
+q = deque()
 
 def in_range(x,y):
     return 0 <= x and x < n and 0 <= y and y < n
 
 def can_go(x,y):
-    return in_range(x,y) and arr[x][y] == 0 and check[x][y]
+    return in_range(x,y) and arr[x][y] == 0 and check[x][y] == False
+
+def bfs():
+    dxs , dys = [1,0,-1,0], [0,-1,0,1]
+    while q:
+        x,y = q.popleft()
+        
+        for dx , dy in zip(dxs,dys):
+            new_x , new_y  = x + dx , y + dy
+
+            if can_go(new_x,new_y):
+                push(new_x,new_y)
+
+def push(x,y):
+    global order
+    answer[x][y] = order
+    order +=1
+    check[x][y] = True
+    q.append((x,y))
+
+for x,y in position:
+    start_x , start_y = x-1 , y-1
+    if(check[start_x][start_y] == False):
+        push(start_x,start_y)
+        bfs()
+
+print(order)
