7cccb87c25125e9c502aeb47f8115fcfe4de465c
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-08-27 14:25:19 +0900

    Title: 최소 와이파이 수, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240827/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240827/최소 와이파이 수/minimum-number-of-wifi.py
@@ -1,18 +1,20 @@
-n,m = map(int,input().split())
-people = list(map(int,input().split()))
+n, m = map(int, input().split())
+people = list(map(int, input().split()))
 
 i = 0
 cnt = 0
 
-
 while i < n:
-    if (people[i] == 1):
-        wifi = i
-        for k in range(i+1,min(i+m+1,n)):
-            if(people[k] == 1):
-                wifi = k
-        cnt +=1
-        i = wifi + m + 1
+    if people[i] == 1:
+        # 사람이 있는 곳부터 오른쪽으로 이동하면서 가장 멀리 있는 사람을 찾음
+        install_pos = i  # 와이파이를 설치할 위치
+        for k in range(i + 1, min(i + m + 1, n)):
+            if people[k] == 1:
+                install_pos = k
+        # 찾은 위치에 와이파이 설치
+        cnt += 1
+        i = install_pos + (2 * m) + 1  # 와이파이 커버 범위 이후로 이동
     else:
         i += 1
+
 print(cnt)
