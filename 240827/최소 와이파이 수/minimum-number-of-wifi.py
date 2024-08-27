f092781e82ebce60b51109207100e384791994df
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-08-27 14:19:01 +0900

    Title: 최소 와이파이 수, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240827/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240827/최소 와이파이 수/minimum-number-of-wifi.py
@@ -1,16 +1,14 @@
 n,m = map(int,input().split())
 people = list(map(int,input().split()))
 
-ans = n // (2*m +1)
-
 i = 0
 cnt = 0
 
 
-while i < len(people):
+while i < n:
     if (people[i] == 1):
         wifi = i
-        for k in range(i+1,min(i+m+1,n+1)):
+        for k in range(i+1,min(i+m+1,n)):
             if(people[k] == 1):
                 wifi = k
         cnt +=1
