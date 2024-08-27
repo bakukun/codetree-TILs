8c98263b3424af3c8c1fc5d789304f0e9f662974
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-08-27 14:17:58 +0900

    Title: 최소 와이파이 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240827/최소 와이파이 수/minimum-number-of-wifi.py
+++ 240827/최소 와이파이 수/minimum-number-of-wifi.py
@@ -2,7 +2,19 @@ n,m = map(int,input().split())
 people = list(map(int,input().split()))
 
 ans = n // (2*m +1)
-if(ans == 0):
-    print(1)
-else:
-    print(ans)
+
+i = 0
+cnt = 0
+
+
+while i < len(people):
+    if (people[i] == 1):
+        wifi = i
+        for k in range(i+1,min(i+m+1,n+1)):
+            if(people[k] == 1):
+                wifi = k
+        cnt +=1
+        i = wifi + m + 1
+    else:
+        i += 1
+print(cnt)
