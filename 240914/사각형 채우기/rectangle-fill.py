b8383dafc7a65d17bafa8e9e1471e9d9555863cb
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-09-14 20:17:56 +0900

    Title: 사각형 채우기, Time: 140ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240914/사각형 채우기/rectangle-fill.py
@@ -0,0 +1,18 @@
+n = int(input())
+memo = [-1] * (n+1)
+
+def fibo(x):
+    if memo[x] != -1:
+        return memo[x] 
+    if x == 1:               
+        memo[x] = 1   
+    elif x == 2:               
+        memo[x] = 2   
+    else:
+        memo[x] = fibo(x-1) + fibo(x-2)
+    return memo[x]
+
+
+fibo(n)
+
+print(memo[n])
