81121703d8865062812ceb0d28293b91efbaf15a
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-09-14 20:09:40 +0900

    Title: 계단 오르기, Time: 171ms, Memory: 73MB, Status: Passed - CodeTree

--- 240914/계단 오르기/climbing-stairs.py
+++ 240914/계단 오르기/climbing-stairs.py
@@ -3,13 +3,13 @@ cnt = [-1]  * (n+1)
 
 def stair(x):
     if cnt[x] != -1:
-        return cnt[x] 
+        return cnt[x] % 10007
     if  x <= 4:
         cnt[x] = 1
     else:
-        cnt[x] = stair(x-2) + stair(x-3)
+        cnt[x] = (stair(x-2) + stair(x-3)) % 10007
    
-    return cnt[x]
+    return (cnt[x])
 
 stair(n)
 print(cnt[n])
