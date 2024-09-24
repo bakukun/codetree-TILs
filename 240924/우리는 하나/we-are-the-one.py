42fd9c5464caeb8764730c5ba50b3635288987ce
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-09-24 20:50:36 +0900

    Title: 우리는 하나, Time: 168ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240924/우리는 하나/we-are-the-one.py
@@ -0,0 +1,63 @@
+from collections import deque
+from itertools import combinations 
+
+n, k, u, d = map(int,input().split())
+arr = [list(map(int,input().split())) for _ in range(n)]
+
+dxs = [1,0,-1,0]
+dys = [0,1,0,-1]
+cnt = 0
+
+def in_range(x,y):
+    return 0 <= x < n and 0 <= y < n
+
+
+
+# 현재 위치에서 차이나는거 몇개 갈수 있는지 카운트 
+# 갈수 있는 것들 visited 반환
+
+def bfs(x,y):
+    visited = [[False] * n for _ in range(n)]
+    q = deque()
+    q.append((x,y))
+    visited[x][y] = True
+    cnt = 1
+
+    while q:
+        dx , dy = q.popleft()
+
+        for i , j in zip(dxs,dys):
+            nx = i + dx
+            ny = j + dy
+
+            if(in_range(nx,ny)):
+                minus = abs(arr[dx][dy] - arr[nx][ny]) 
+
+                if (not visited[nx][ny] and u <= minus <= d):
+                    cnt += 1
+                    visited[nx][ny] = True
+                    q.append((nx,ny))
+    return cnt
+
+maximum = 0
+
+
+coords = [(i, j) for i in range(n) for j in range(n)]
+comb = list(combinations(coords, k))
+
+
+for num in comb:
+    if(k == 1):
+        x , y = num[0]
+        maximum = max(maximum, bfs(x,y))
+    elif(k == 2):
+        x1 , y1 = num[0]
+        x2 , y2 = num[1]
+        maximum = max(maximum, bfs(x1,y1)+bfs(x2,y2))
+    else:
+        x1 , y1 = num[0]
+        x2 , y2 = num[1]
+        x3 , y3 = num[2]
+        maximum = max(maximum, bfs(x1,y1) + bfs(x2,y2) + bfs(x3,y3))
+        
+print(maximum)
