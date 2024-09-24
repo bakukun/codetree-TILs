9a1774f4417e956f6cbbf1a5f31c6e56d3468fba
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-09-24 21:08:47 +0900

    Title: 우리는 하나, Time: 181ms, Memory: 25MB, Status: Passed - CodeTree

--- 240924/우리는 하나/we-are-the-one.py
+++ 240924/우리는 하나/we-are-the-one.py
@@ -1,46 +1,51 @@
 from collections import deque
+from itertools import combinations 
 
-n, k, u, d = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
-dxs = [1, 0, -1, 0]
-dys = [0, 1, 0, -1]
 
-def in_range(x, y):
+n, k, u, d = map(int,input().split())
+arr = [list(map(int,input().split())) for _ in range(n)]
+dxs = [1,0,-1,0]
+dys = [0,1,0,-1]
+size = []
+cnt = 0
+visited = [[False] * n for _ in range(n)]
+
+def in_range(x,y):
     return 0 <= x < n and 0 <= y < n
 
-visited = [[False] * n for _ in range(n)]
-component_sizes = []
 
-def bfs(x, y):
+
+# 현재 위치에서 차이나는거 몇개 갈수 있는지 카운트 
+# 갈수 있는 것들 visited 반환
+
+def bfs(x,y):
     q = deque()
-    q.append((x, y))
+    q.append((x,y))
+
     visited[x][y] = True
     cnt = 1
 
     while q:
-        dx, dy = q.popleft()
+        dx , dy = q.popleft()
 
-        for i, j in zip(dxs, dys):
-            nx = dx + i
-            ny = dy + j
+        for i , j in zip(dxs,dys):
+            nx = i + dx
+            ny = j + dy
 
-            if in_range(nx, ny) and not visited[nx][ny]:
-                diff = abs(arr[dx][dy] - arr[nx][ny])
-                if u <= diff <= d:
+            if(in_range(nx,ny)):
+                minus = abs(arr[dx][dy] - arr[nx][ny]) 
+
+                if (not visited[nx][ny] and u <= minus <= d):
                     cnt += 1
                     visited[nx][ny] = True
-                    q.append((nx, ny))
+                    q.append((nx,ny))
     return cnt
 
-# Find all connected components
+
 for i in range(n):
     for j in range(n):
-        if not visited[i][j]:
-            size = bfs(i, j)
-            component_sizes.append(size)
+        size.append(bfs(i,j))
 
-# Get the sum of the sizes of the largest k components
-component_sizes.sort(reverse=True)
-maximum = sum(component_sizes[:k])
-
-print(maximum)
+size.sort(reverse=True)
+        
+print(sum(size[:k]))
