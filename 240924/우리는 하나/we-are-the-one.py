c259022ce57e639e8f211f6e13d59867c0de2079
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-09-24 21:03:21 +0900

    Title: 우리는 하나, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

--- 240924/우리는 하나/we-are-the-one.py
+++ 240924/우리는 하나/we-are-the-one.py
@@ -1,91 +1,46 @@
 from collections import deque
-from itertools import combinations 
 
-n, k, u, d = map(int,input().split())
-arr = [list(map(int,input().split())) for _ in range(n)]
-dxs = [1,0,-1,0]
-dys = [0,1,0,-1]
-cnt = 0
+n, k, u, d = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+dxs = [1, 0, -1, 0]
+dys = [0, 1, 0, -1]
 
-def in_range(x,y):
+def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
+visited = [[False] * n for _ in range(n)]
+component_sizes = []
 
-
-# 현재 위치에서 차이나는거 몇개 갈수 있는지 카운트 
-# 갈수 있는 것들 visited 반환
-
-def bfs(x,y):
+def bfs(x, y):
     q = deque()
-    q.append((x,y))
-    visited = [[False] * n for _ in range(n)]
+    q.append((x, y))
     visited[x][y] = True
     cnt = 1
 
     while q:
-        dx , dy = q.popleft()
+        dx, dy = q.popleft()
 
-        for i , j in zip(dxs,dys):
-            nx = i + dx
-            ny = j + dy
+        for i, j in zip(dxs, dys):
+            nx = dx + i
+            ny = dy + j
 
-            if(in_range(nx,ny)):
-                minus = abs(arr[dx][dy] - arr[nx][ny]) 
-
-                if (not visited[nx][ny] and u <= minus <= d):
+            if in_range(nx, ny) and not visited[nx][ny]:
+                diff = abs(arr[dx][dy] - arr[nx][ny])
+                if u <= diff <= d:
                     cnt += 1
                     visited[nx][ny] = True
-                    q.append((nx,ny))
-    return visited, cnt
+                    q.append((nx, ny))
+    return cnt
 
+# Find all connected components
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            size = bfs(i, j)
+            component_sizes.append(size)
 
-def bfs2(x,y,visited):
-    q = deque()
-    q.append((x,y))
-    visited[x][y] = True
-    cnt = 1
+# Get the sum of the sizes of the largest k components
+component_sizes.sort(reverse=True)
+maximum = sum(component_sizes[:k])
 
-    while q:
-        dx , dy = q.popleft()
-
-        for i , j in zip(dxs,dys):
-            nx = i + dx
-            ny = j + dy
-
-            if(in_range(nx,ny)):
-                minus = abs(arr[dx][dy] - arr[nx][ny]) 
-
-                if (not visited[nx][ny] and u <= minus <= d):
-                    cnt += 1
-                    visited[nx][ny] = True
-                    q.append((nx,ny))
-    return visited, cnt
-
-maximum = 0
-
-
-coords = [(i, j) for i in range(n) for j in range(n)]
-comb = list(combinations(coords, k))
-
-
-for num in comb:
-    if(k == 1):
-        x , y = num[0]
-        visited , tmp = bfs(x,y)
-        maximum = max(maximum,tmp)
-    elif(k == 2):
-        x1 , y1 = num[0]
-        x2 , y2 = num[1]
-        visited , tmp = bfs(x1,x2) 
-        visited , tmp2 = bfs2(x2,y2,visited)
-        maximum = max(maximum, tmp + tmp2)
-    else:
-        x1 , y1 = num[0]
-        x2 , y2 = num[1]
-        x3 , y3 = num[2]
-        visited , tmp = bfs(x1,x2) 
-        visited , tmp2 = bfs2(x2,y2,visited)
-        visited , tmp3 = bfs2(x3,y3,visited)
-        maximum = max(maximum,  tmp + tmp2 + tmp3)
-        
 print(maximum)
