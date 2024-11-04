5a13ffc35300eaf8da515d118d94a231ab6689e5
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-11-04 18:29:14 +0900

    Title: xor 결과 최대 만들기, Time: 80ms, Memory: 19MB, Status: Passed - CodeTree

--- 241104/xor 결과 최대 만들기/max-of-xor.py
+++ 241104/xor 결과 최대 만들기/max-of-xor.py
@@ -1,24 +1,25 @@
 def xor(ans):
     tmp = ans[0]
-    for i in range(1,len(ans)):
+    for i in range(1, len(ans)):
         tmp = tmp ^ ans[i]
     return tmp
 
-def backtracking(cnt):
-    if(cnt == M):
-        maxi = max(maxi,xor(ans))
+
+def backtracking(idx,cnt):
+    global ans,maxi
+    if (cnt == M):
+        maxi = max(maxi, xor(ans))
         return
-    
-    for i in range(arr):
-        ans.append(i)
-        backtracking(cnt+1)
+
+    for i in range(idx,len(arr)):
+        ans.append(arr[i])
+        backtracking(i+1,cnt + 1)
         ans.pop()
 
-        backtracking(cnt)
 
-
-N,M = map(int,input().split())
-arr = list(map(int,input().split())
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
 ans = []
 maxi = 0
-backtracking(0)
+backtracking(0,0)
+print(maxi)
