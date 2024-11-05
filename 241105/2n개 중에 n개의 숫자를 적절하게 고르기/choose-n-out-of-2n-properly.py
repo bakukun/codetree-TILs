d3a07b754974425606d422dd98e3c8deb48e830b
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-11-05 14:07:15 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 43ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241105/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
+++ 241105/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -2,17 +2,29 @@ def backtracking(idx,cnt):
     global ans,mini
 
     if cnt == N:
-        other = list(set_nlist - set(ans))
 
-        if(len(other) == len(ans)):
-            mini = min(mini,abs(sum(ans)-sum(other)))
-        return
+        other = []
+        n_visited = [False] * len(n_list)
+        sum_ans = 0
+        sum_other = 0
+
+        for n_idx in ans:
+            n_visited[n_idx] = True
+            sum_ans += n_list[n_idx]
+
+        for i in range(len(n_visited)):
+            if not n_visited[i]:
+                sum_other += n_list[i]
+
+        #print(ans,other)
+        mini = min(mini,abs(sum_ans-sum_other))
+        return 
 
     if(idx == len(n_list)-1):
         return
 
     
-    ans.append(n_list[idx])
+    ans.append(idx)
     backtracking(idx,cnt+1)
     ans.pop()
     
@@ -22,7 +34,8 @@ def backtracking(idx,cnt):
         
 N = int(input())
 n_list = list(map(int,input().split()))
-set_nlist = set(n_list)
+
+
 ans = []
 mini = 10000000000
 backtracking(0,0)
