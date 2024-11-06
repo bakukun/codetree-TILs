e7b354d0a6fe03f93f32e60b2392062d678302dd
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-11-06 17:09:35 +0900

    Title: 최고의 33위치, Time: 43ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241106/최고의 33위치/best-place-of-33.py
+++ 241106/최고의 33위치/best-place-of-33.py
@@ -3,8 +3,8 @@ arr = [list(map(int,input().split())) for _ in range(n)]
 maxi = 0
 
 for i in range(n-2):
-    tmp = 0
     for j in range(n-2):
+        tmp = 0
         for k in range(3):
             for l in range(3):
                 tmp += arr[i+k][j+l]
