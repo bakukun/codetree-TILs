3b83e912107d5e274da1bb53bd2390c2bfdd85ff
JINSOO HONG <50573818+bakukun@users.noreply.github.com>
2024-11-06 17:08:19 +0900

    Title: 최고의 33위치, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241106/최고의 33위치/best-place-of-33.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = [list(map(int,input().split())) for _ in range(n)]
+maxi = 0
+
+for i in range(n-2):
+    tmp = 0
+    for j in range(n-2):
+        for k in range(3):
+            for l in range(3):
+                tmp += arr[i+k][j+l]
+    maxi = max(maxi,tmp)
+        
+print(maxi)
